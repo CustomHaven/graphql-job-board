1faf60c72c2c7b4afed6650e667ab8206232c792
CustomHaven <mmuseaden@gmail.com>
2025-09-27 10:49:41 +0100

    feat: real error handlings added

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,23 @@
+import {  GraphQLError } from "graphql";
 import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
 import { getCompany } from "./db/companies.js";
 
 export const resolvers = {
     Query: {
-        company: async (_root, args) => await getCompany(args.id),
-        job: async (_root, args) => await getJob(args.id),
+        company: async (_root, args) => {
+            const company = await getCompany(args.id);
+            if (!company) {
+                throw notFoundError("No Company found with id " + args.id);
+            }
+            return company;
+        },
+        job: async (_root, args) => {
+            const job = await getJob(args.id)
+            if (!job) {
+                throw notFoundError("No Job found with id " + args.id);
+            }
+            return job;
+        },
         jobs: async () => await getJobs(),
     },
 
@@ -20,4 +33,12 @@ export const resolvers = {
 
 function toISODate(value) { // value.replace(/T.*/, "");
     return value.slice(0, "yyyy-mm-dd".length);
+}
+
+function notFoundError(message) {
+    return new GraphQLError(message, {
+        extensions: {
+            code: "NOT_FOUND",
+        }
+    });
 }
